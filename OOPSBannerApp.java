b64df9d6542491d68a7f33231893587cd050173e
Rithika Mahesh <rithikamahesh97@gmail.com>
2026-03-02 13:47:40 +0530

    UC3: Refactor ASCII banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,34 @@
 /**
  * OOPSBannerApp
- * Displays OOPS in ASCII Banner format.
+ * Displays OOPS banner using String.join() for better memory handling.
  *
  * @author Rithika Mahesh
- * @version 2.0
+ * @version 3.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *****   *****  ");
-        System.out.println("*     * *     * *        *     ");
-        System.out.println("*     * *     * *        *     ");
-        System.out.println(" *****   *****  *        *****  ");
+        System.out.println(String.join(" ",
+                "*****", "*****", "*****", "*****"));
 
+        System.out.println(String.join(" ",
+                "*", "   *", "*", "   *", "*", "   *", "*", "   *"));
+
+        System.out.println(String.join(" ",
+                "*", "   *", "*", "   *", "*", "   *", "*", "   *"));
+
+        System.out.println(String.join(" ",
+                "*", "   *", "*", "   *", "*****", "*****"));
+
+        System.out.println(String.join(" ",
+                "*", "   *", "*", "   *", "*", "", "*"));
+
+        System.out.println(String.join(" ",
+                "*", "   *", "*", "   *", "*", "", "*"));
+
+        System.out.println(String.join(" ",
+                "*****", "*****", "*", "", "*****"));
     }
 }
